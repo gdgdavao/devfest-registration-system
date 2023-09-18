ecbb908ebcdb5a29c4ae672c97512e02820000ce
Ned Palacios <npdoesmc@gmail.com>
2023-09-18 15:35:49 +0800

    fix: oops add to commit checkbox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { pb } from "@/client";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger } from "@/components/ui/select";
