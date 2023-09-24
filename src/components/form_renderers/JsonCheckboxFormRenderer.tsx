d8d0d6dc8bd699365a5ad2bdc851ea8ef83eb102
Ned Palacios <npdoesmc@gmail.com>
2023-09-24 19:54:25 +0800

    feat: add JsonCheckboxFormRenderer

---
+++ src/components/form_renderers/JsonCheckboxFormRenderer.tsx
@@ -0,0 +1,26 @@
+import { FormFieldRendererProps } from "../FormFieldRenderer";
+import { FormControl, FormItem, FormLabel } from "../ui/form";
+import { Checkbox } from "../ui/checkbox";
+
+export default function JsonCheckboxFormRenderer({ name, value = [], field, onChange }: FormFieldRendererProps) {
+    const values = field.options.values as string[];
+
+    return <>
+        {(values ?? []).map((v) => (
+            <FormItem
+                key={`${name}_${v}`}
+                className="flex flex-row items-start space-x-3 space-y-0">
+                <FormControl>
+                    <Checkbox
+                            checked={value.includes(v)}
+                            onCheckedChange={(checked) => {
+                                return checked
+                                    ? onChange(value.concat(v))
+                                    : onChange(value.filter((val: string) => val !== v))
+                            }} />
+                </FormControl>
+                <FormLabel>{v}</FormLabel>
+            </FormItem>
+        ))}
+    </>
+}
