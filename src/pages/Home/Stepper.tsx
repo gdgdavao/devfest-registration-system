8a55910cb0597ce7e33a92d3eb222d50b5ebc920
Ned Palacios <npdoesmc@gmail.com>
2023-10-01 19:06:44 +0800

    fix: display only items that have show in stepper enabled

--- src/pages/Home/Stepper.tsx
+++ src/pages/Home/Stepper.tsx
@@ -6,25 +6,26 @@ const Stepper = ({ index }: { index: number }) => {
 
     return (
         <div className="flex justify-center space-x-5 md:space-x-10 mb-8 md:mb-12">
-            {groups?.map((group, sIdx) => {
-                return (
-                    <div
-                        key={sIdx + 1}
-                        className="flex flex-col items-center text-sm gap-y-2"
-                    >
+            {(groups ?? []).filter(g => g.show_in_stepper)
+                .map((group, sIdx) => {
+                    return (
                         <div
-                            className={cn(
-                                "h-8 w-8 md:w-12 md:h-12 rounded-full bg-secondary flex items-center justify-center",
-                                index - 1 === sIdx &&
-                                    "bg-primary text-secondary"
-                            )}
+                            key={sIdx + 1}
+                            className="flex flex-col items-center text-sm gap-y-2"
                         >
-                            {sIdx + 1}
+                            <div
+                                className={cn(
+                                    "font-mono h-8 w-8 md:w-12 md:h-12 rounded-full bg-secondary flex items-center justify-center",
+                                    index - 1 === sIdx &&
+                                        "bg-primary text-secondary"
+                                )}
+                            >
+                                {sIdx + 1}
+                            </div>
+                            {group.short_title ?? group.title ?? ''}
                         </div>
-                        {group.short_title ?? group.title ?? ''}
-                    </div>
-                );
-            })}
+                    );
+                })}
         </div>
     );
 };
