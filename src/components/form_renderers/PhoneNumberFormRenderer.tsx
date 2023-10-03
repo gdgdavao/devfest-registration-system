4644b27ce17a92c0038c1ff0655b8fee346f19c7
Ned Palacios <npdoesmc@gmail.com>
2023-10-03 18:48:38 +0800

    feat: add placeholder support in PhoneNumberFormRenderer

--- src/components/form_renderers/PhoneNumberFormRenderer.tsx
+++ src/components/form_renderers/PhoneNumberFormRenderer.tsx
@@ -2,7 +2,7 @@ import { FormFieldRendererProps } from "../FormFieldRenderer";
 import PhoneFormatter from "react-headless-phone-input";
 import { Input } from "../ui/input";
 
-export default function PhoneNumberFormRenderer({ value, onChange }: FormFieldRendererProps) {
+export default function PhoneNumberFormRenderer({ value, field, onChange }: FormFieldRendererProps) {
     return (
         <PhoneFormatter defaultCountry="PH" value={value} onChange={onChange}>
             {({ inputValue, onInputChange, onBlur, impossible }) => (<>
@@ -10,6 +10,7 @@ export default function PhoneNumberFormRenderer({ value, onChange }: FormFieldRe
                     type="tel"
                     value={inputValue}
                     onBlur={onBlur}
+                    placeholder={field.options.placeholder as string | undefined}
                     onChange={(e) => onInputChange(e.target.value)} />
                 {impossible && <p className="text-destructive">Invalid phone number</p>}
             </>)}
