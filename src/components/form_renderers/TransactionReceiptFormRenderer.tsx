d3294ec6f7de382cafa8ed14c09ad48d87c191a5
Ned Palacios <npdoesmc@gmail.com>
2023-10-04 20:49:05 +0800

    fix: use dynamic fields in TransactionReceiptFormRenderer

--- src/components/form_renderers/TransactionReceiptFormRenderer.tsx
+++ src/components/form_renderers/TransactionReceiptFormRenderer.tsx
@@ -4,34 +4,11 @@ import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessa
 import { useFormContext } from "react-hook-form";
 
 export default function TransactionReceiptFormRenderer({ field }: FormFieldRendererProps) {
-    const fields: RegistrationField[] = [
-        {
-            name: 'transaction_id',
-            title: 'Transaction ID',
-            group: '',
-            description: '',
-            type: 'text',
-            options: {
-                required: true
-            }
-        },
-        {
-            name: `${field.name}.mobile_number`,
-            title: 'GCash Mobile Number',
-            group: '',
-            description: '',
-            type: 'text',
-            options: {
-                required: true
-            }
-        }
-    ];
-
     const form = useFormContext();
 
     return (
         <div className="flex flex-col space-y-4">
-            {fields.map((sfield) => (
+            {(field.options.fields as RegistrationField[]).map((sfield) => (
                 <FormField
                     key={`${field.name}.${sfield.name}`}
                     control={form.control}
