46c52796eaf2a6d2c4a0a5f4356732ec5158d966
Arnel Jan Sarmiento <aesarmiento@up.edu.ph>
2023-10-05 19:16:53 +0800

    feat: improve admin registration dialog (#28)
    * feat: added bio and addon fields in dialog box
    
    * feat: added student, payment and profesional details
    
    * fix: condtional yes or no on is fresh grad
    
    * feat: added payment details and registration date
    
    ---------
    
    Co-authored-by: Ned Palacios <npdoesmc@gmail.com>

--- src/pages/admin/registrations/ScreenRegistrantDialog.tsx
+++ src/pages/admin/registrations/ScreenRegistrantDialog.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { useRegistrationQuery, useUpdateRegistrationStatusMutation } from "@/client";
+import { useRegistrationQuery, pb, useUpdateRegistrationStatusMutation } from "@/client";
 import { RegistrationStatusesStatusOptions } from "@/pocketbase-types";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
@@ -38,10 +38,113 @@ export default function ScreenRegistrantDialog({ id, children }: { id: string, c
                     </Button>
                 </div>
 
-                <div className="flex flex-col py-8">
-                    <span className="text-slate-500">Name</span>
-                    <p className="text-2xl font-bold">{registrant?.last_name}, {registrant?.first_name}</p>
+                <div className="flex flex-row py-8 justify-center items-end">
+                    <div className="flex-1 flex flex-col">
+                        <span className="text-slate-500">Name</span>
+                        <p className="text-2xl font-bold">{registrant?.last_name}, {registrant?.first_name}</p>
+                        <p className="font-bold">
+                            {
+                                registrant?.type == "student" ?
+                                <span>Student</span>: 
+                                <span>Professional</span>
+                            }
+                        </p>
+                    </div>
+                    <div className="flex-1 flex flex-col">
+                        <span className="text-slate-500">Ticket Type</span>
+                        <p className="font-bold">{registrant?.expand?.ticket?.name}</p>
+                        <p className="font-bold">
+                            {
+                                registrant?.created &&
+                                new Date(registrant?.created).toLocaleString()
+                            }
+                        </p>
+                    </div>
+                    <div className="flex-1 flex flex-col">
+                        <span className="text-slate-500">Payment Status</span>
+                        <p className="font-bold">{registrant?.expand?.payment?.status}</p>
+                        <p className="font-bold">
+                            {
+                                registrant?.expand?.payment?.expected_amount && 
+                                <span>₱{registrant.expand.payment.expected_amount}</span>
+                            }
+                        </p>
+                    </div>
                 </div>
+
+                <div className="flex flex-col space-y-2 py-4">
+                    <span className="text-slate-500">Bio</span>
+
+                    <div className="flex flex-row pt-4">
+                        <div className="flex-1 flex flex-col">
+                            <span className="text-slate-500">Gender</span>
+                            <p className="font-bold">{registrant?.sex}</p>
+                        </div>
+
+                        <div className="flex-1 flex flex-col">
+                            <span className="text-slate-500">Age Range</span>
+                            <p className="font-bold">{registrant?.age_range}</p>
+                        </div>
+
+                        <div className="flex-1 flex flex-col">
+                            <span className="text-slate-500">Years of Tech Experience</span>
+                            <p className="font-bold">{registrant?.years_tech_exp}</p>
+                        </div>
+                    </div>
+                </div>
+                
+                {
+                    registrant?.expand?.student_profile &&
+                    <div className="flex flex-col space-y-2 py-4">
+                        <span className="text-slate-500">Student Profile</span>
+
+                        <div className="flex flex-row pt-4">
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">School</span>
+                                <p className="font-bold">{registrant?.expand?.student_profile?.school}</p>
+                            </div>
+
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">Designation</span>
+                                <p className="font-bold">{registrant?.expand?.student_profile?.designation}</p>
+                            </div>
+
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">Year Level</span>
+                                <p className="font-bold">{registrant?.expand?.student_profile?.year_level}</p>
+                            </div>
+                        </div>
+                    </div> 
+                }
+                {
+                    registrant?.expand?.professional_profile &&
+                    <div className="flex flex-col space-y-2 py-4">
+                        <span className="text-slate-500">Professional Profile</span>
+
+                        <div className="flex flex-row pt-4">
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">Is Fresh Graduate?</span>
+                                <p className="font-bold">
+                                    {
+                                        registrant?.expand?.professional_profile?.is_fresh_graduate ?
+                                        <span>Yes</span> :
+                                        <span>No</span>
+                                    }
+                                </p>
+                            </div>
+
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">Organization</span>
+                                <p className="font-bold">{registrant?.expand?.professional_profile?.organization}</p>
+                            </div>
+
+                            <div className="flex-1 flex flex-col">
+                                <span className="text-slate-500">Title</span>
+                                <p className="font-bold">{registrant?.expand?.professional_profile?.title}</p>
+                            </div>
+                        </div>
+                    </div> 
+                }
 
                 <div className="flex flex-col space-y-2 py-4">
                     <span className="text-slate-500">Contact Details</span>
@@ -59,19 +162,44 @@ export default function ScreenRegistrantDialog({ id, children }: { id: string, c
                     </div>
                 </div>
 
-                <div className="flex flex-col space-y-2 py-4">
-                    <span className="text-slate-500">Add-on Details</span>
-
-                    <div className="flex flex-row pt-4">
-                        <div className="flex-1 flex flex-col">
-                            <span className="text-slate-500">Selected add-ons</span>
-                            <p className="font-bold">{registrant?.expand?.addons.addon}</p>
-                        </div>
-
-                        <div className="flex-1 flex flex-col">
-                            <span className="text-slate-500">Contact Number</span>
-                            <p className="font-bold">{registrant?.contact_number}</p>
-                        </div>
+                <div className="space-y-2 py-4">
+                    <span className="text-slate-500">Selected add-ons</span>
+                    <div className="flex flex-col divide-y-2">
+                        {
+                            registrant?.expand?.addons?.map(addon => {
+                                return (
+                                    <div className="pt-4">
+                                        <div className="flex text-slate-500">Add-on Details</div>
+                                        <div className="flex flex-row justify-around py-2 ">
+                                            <div className="flex flex-col">
+                                                <span className="text-slate-500">Name</span>
+                                                <p className="font-bold">
+                                                    {addon?.expand?.addon.title}
+                                                </p>
+                                                {
+                                                    addon?.preferences != undefined &&
+                                                    Object.entries(addon?.preferences).map(([key, value], id) => (
+                                                        <span key={id}>
+                                                            <span>{key}:</span> {value}
+                                                        </span>
+                                                    ))
+                                                }
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <img
+                                                    src={pb.files.getUrl(addon?.expand?.addon, addon?.expand?.addon.cover_image)}
+                                                    className="rounded-md object-cover h-20 w-auto border"
+                                                    alt={addon?.expand?.addon.title} />
+                                            </div>
+                                            <div className="flex flex-col">
+                                                <span className="text-slate-500">Price</span>
+                                                <p className="font-bold">{addon?.expand?.addon.price}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
