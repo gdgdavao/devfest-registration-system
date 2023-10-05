918a33105f3bfacd5e52b2bce37974e3074eef35
Ned Palacios <npdoesmc@gmail.com>
2023-10-05 16:48:10 +0800

    fix(pb): using wrong variable inside getRegistrationFields

--- pb/pb_hooks/utils.js
+++ pb/pb_hooks/utils.js
@@ -69,7 +69,7 @@ module.exports = {
         }
 
         if (!$app.cache().has(`registration_fields_${type}`)) {
-            utils.buildRegistrationFields();
+            this.buildRegistrationFields();
         }
 
         return $app.cache().get(`registration_fields_${type}`);
