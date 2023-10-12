04636383e2effe0446b4876ea3daf95868f1b469
Ned Palacios <npdoesmc@gmail.com>
2023-10-12 17:45:04 +0800

    feat: add minimal versions of form renderers

--- src/pages/admin/registrations/NewRegistrationDialog.tsx
+++ src/pages/admin/registrations/NewRegistrationDialog.tsx
@@ -1,6 +1,11 @@
 // import { useRegistrationMutation } from "@/client";
 import { useRegistrationMutation } from "@/client";
 import RegistrationForm from "@/components/RegistrationForm";
+import ExpectedAmountFormRenderer from "@/components/form_renderers/ExpectedAmountFormRenderer";
+import MinimalRichAddonsFormRenderer from "@/components/form_renderers/MinimalRichAddonsFormRenderer";
+import MinimalRichTicketFormRenderer from "@/components/form_renderers/MinimalRichTicketsFormRenderer";
+import MinimalTopicInterestFormRenderer from "@/components/form_renderers/MinimalTopicInterestFormRenderer";
+import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Form } from "@/components/ui/form";
 import { RegistrationFormContext, useSetupRegistrationForm } from "@/registration-form";
@@ -16,13 +21,25 @@ export default function NewRegistrationDialog({ children }: { children: ReactNod
 
     return <Dialog>
         <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent className="lg:max-w-screen-md overflow-y-scroll max-h-[calc(100vh-2rem)]">
+        <DialogContent className="lg:max-w-screen-xl overflow-y-scroll max-h-[calc(100vh-2rem)]">
             <DialogHeader>
-                <DialogTitle>Register new person</DialogTitle>
+                <DialogTitle>Register new registrant</DialogTitle>
                     <Form {...context.form}>
-                        <form onSubmit={context.form.handleSubmit(() => context.onFormSubmit(context.form.getValues()))}>
+                        <form
+                            onSubmit={context.form.handleSubmit(() => context.onFormSubmit(context.form.getValues()))}>
                             <RegistrationFormContext.Provider value={context}>
-                                <RegistrationForm />
+                                <RegistrationForm
+                                    className="max-w-2xl mx-auto"
+                                    customComponents={{
+                                        topic_interests: MinimalTopicInterestFormRenderer,
+                                        ticket: MinimalRichTicketFormRenderer,
+                                        addons_data: MinimalRichAddonsFormRenderer,
+                                        "payment_data.expected_amount": ExpectedAmountFormRenderer,
+                                    }} />
+
+                                <div className="py-4 flex justify-end sticky bottom-[-22px] border-t bg-white">
+                                    <Button>Save</Button>
+                                </div>
                             </RegistrationFormContext.Provider>
                         </form>
                     </Form>
