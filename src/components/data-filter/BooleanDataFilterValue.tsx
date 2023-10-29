d22aa876d1d4d9bcf47876c9d34caf6e4cde28e5
Ned Palacios <npdoesmc@gmail.com>
2023-10-29 18:29:29 +0800

    fix: boolean filter not working

--- src/components/data-filter/BooleanDataFilterValue.tsx
+++ src/components/data-filter/BooleanDataFilterValue.tsx
@@ -2,10 +2,10 @@ import SelectDataFilterValue from "./SelectDataFilterValue";
 
 export default function BooleanSelectDataFilterValue({ value, onChange }: {
     value: string
-    onChange: (s: string) => void
+    onChange: (s: boolean) => void
 }) {
     return <SelectDataFilterValue
-        value={value}
-        onChange={onChange}
+        value={value ? "true" : "false"}
+        onChange={(v) => onChange(v === "true")}
         values={["true", "false"]} />;
 }
